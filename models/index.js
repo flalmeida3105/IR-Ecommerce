81356af1b94ef0ed9bfe0a9141b4c28a673dec0a
Fernando Almeida <nando.lalmeida@gmail.com>
2022-06-14 20:02:16 -0400

    added initial models configuration

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+ProductTag.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag
+});
 
 module.exports = {
   Product,
@@ -18,3 +26,39 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+// // import models
+// const Product = require('./Product');
+// const Category = require('./Category');
+// const Tag = require('./Tag');
+// const ProductTag = require('./ProductTag');
+
+// // Products belongsTo Category
+// Product.belongsTo(Category, {
+//   foreignKey: "category_id",
+// });
+
+// // Categories have many Products
+// Category.hasMany(Product, {
+//   foreignKey: "category_id",
+// });
+
+// // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(Tag, {
+//   through: ProductTag,
+//   foreignKey: "product_id",
+// });
+
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   through: ProductTag,
+//   foreignKey: 'tag_id',
+// });
+
+// module.exports = {
+//   Product,
+//   Category,
+//   Tag,
+//   ProductTag,
+// };
